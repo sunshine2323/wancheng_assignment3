72009996e32c8205d3520871e45decd16cb7b9d0
sunshine2323 <wsun@unomaha.edu>
2018-04-02 23:59:46 -0500

    first commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    tone_analyzer = ToneAnalyzerV3(
+        username='9940b51c-ce83-4dec-9532-52180fb08790',
+        password='TfTvnCiuI7rA',
+        version='2016-05-19 ')
+
+
+    language_translator = LanguageTranslator(
+        username='5d7260ef-2c10-410d-9884-5c0c6dd26085',
+        password='R7Vt4I6SGl48')
+
+    # print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+    My_posts=[]
+    for post in posts:
+        posting = post.text
+        toneObj = json.dumps(tone_analyzer.tone(tone_input=posting,
+                                                content_type="text/plain"), indent=2)
+        post.toneObj2 = json.loads(toneObj)
+        post.angerScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][0]['score']
+        post.disgustScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][1]['score']
+        post.fearScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][2]['score']
+        post.joyScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][3]['score']
+        post.sadScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][4]['score']
+
+
+        translation = language_translator.translate(
+            text=post.text,
+            source='en',
+            target='es')
+        obj= json.dumps(translation, indent=2, ensure_ascii=False)
+        post.obj2 = json.loads(obj)
+
+
+
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
